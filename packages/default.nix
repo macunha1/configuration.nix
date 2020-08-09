e9564093bc269b3e37800abdda62774186166f93
Matheus Cunha <12192633+macunha1@users.noreply.github.com>
2020-08-09 14:54:50 +0200

    feat(awesomewm): DBus proxy as a LGI wrapper
    Including dbus_proxy Lua module to interact with DBus interfaces (plans
    to use in Media Player module, currently Spotify). Knowing that what
    changes is the last entry at org.mpris.MediaPlayer2.NAME, extend support
    to different media players should be easy.
    
    dbus_proxy overlay was heavily copied from author's repository
    https://github.com/stefano-m/nix-stefano-m-nix-overlays, credits to
    stefano-m for the great work.

--- packages/default.nix
+++ packages/default.nix
@@ -1,18 +1,19 @@
 [
-  (self: super: with super; {
-    my = {
-      uhkAgent = (callPackage ./uhk-agent.nix {});
-      cached-nix-shell =
-        (callPackage
-          (builtins.fetchTarball
-            https://github.com/xzfc/cached-nix-shell/archive/master.tar.gz) {});
+  (self: super:
+    with super; {
+      my = {
+        uhkAgent = (callPackage ./uhk-agent.nix { });
+        cached-nix-shell = (callPackage (builtins.fetchTarball
+          "https://github.com/xzfc/cached-nix-shell/archive/master.tar.gz")
+          { });
+        luaDbusProxy = (callPackage ./lua-dbus-proxy.nix);
+      };
 
-    };
-
-    # Bleeding edge all the things
-    unstable = import <nixos-unstable> { inherit config; };
-  })
+      # Bleeding edge all the things
+      unstable = import <nixos-unstable> { inherit config; };
+    })
 
   # emacsGit
-  (import (builtins.fetchTarball https://github.com/nix-community/emacs-overlay/archive/master.tar.gz))
+  (import (builtins.fetchTarball
+    "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz"))
 ]
