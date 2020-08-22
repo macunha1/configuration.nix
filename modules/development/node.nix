0993f9a1da1859a67dd8cfbae143ade8a864d1fb
Matheus Cunha <12192633+macunha1@users.noreply.github.com>
2020-08-22 07:57:32 +0200

    feat(*): Node and Vim module updates
    Vim version bump up to support latest
    [definitely-not-vimrc](https://github.com/macunha1/definitely-not-vimrc)
    with Dein.vim plugin manager;
    
    Node module received proper environment variables for Yarn (to not store
    configurations in $HOME directly) and NodeJS version bump up to latest
    stable.

--- modules/development/node.nix
+++ modules/development/node.nix
@@ -1,10 +1,14 @@
 # modules/development/node.nix --- https://nodejs.org/en/
 
 { config, options, lib, pkgs, ... }:
-with lib;
-{
+with lib; {
   options.modules.development.node = {
     enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+
+    includeBinToPath = mkOption {
       type = types.bool;
       default = false;
     };
@@ -13,16 +17,17 @@ with lib;
   config = mkIf config.modules.development.node.enable {
     my = mkMerge [
       {
-        packages = with pkgs; [
-          nodejs
-          yarn
-        ];
+        packages = with pkgs; [ unstable.nodejs-14_x yarn ];
 
         env.NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/config";
-        env.NPM_CONFIG_CACHE      = "$XDG_CACHE_DIR/npm";
-        env.NPM_CONFIG_TMP        = "$XDG_RUNTIME_DIR/npm";
-        env.NPM_CONFIG_PREFIX     = "$XDG_CACHE_DIR/npm";
-        env.NODE_REPL_HISTORY     = "$XDG_CACHE_DIR/node/repl_history";
+        env.NPM_CONFIG_CACHE = "$XDG_CACHE_DIR/npm";
+        env.NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
+        env.NPM_CONFIG_PREFIX = "$XDG_CACHE_DIR/npm";
+        env.NODE_REPL_HISTORY = "$XDG_CACHE_DIR/node/repl_history";
+
+        # Ref https://yarnpkg.com/configuration/yarnrc
+        env.YARN_CACHE_FOLDER = "$XDG_CACHE_DIR/node/yarn";
+        env.YARN_RC_FILENAME = "$XDG_CONFIG_DIR/node/yarnrc";
 
         home.xdg.configFile."npm/config".text = ''
           cache=$XDG_CACHE_DIR/npm
