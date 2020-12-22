a88641e9fc83c1c71f5f784d5362f966c491ae95
Matheus Cunha <matheuz.a@gmail.com>
2020-12-22 22:18:03 +0100

    feat(orbit): World meet Orbit
    Adding the orbit host which is responsible for configuring a portable
    Raspberry PI Linux setup (quite like a laptop).

---
+++ hosts/orbit/modules.nix
@@ -0,0 +1,87 @@
+{ config, lib, pkgs, options, ... }:
+
+{
+  modules = {
+    desktop = {
+      awesomewm.enable = true;
+
+      terminal = {
+        default = "alacritty";
+        alacritty.enable = true;
+      };
+
+      applications = {
+        redshift.enable = true;
+
+        rofi = {
+          enable = true;
+          theme = "yin-yang";
+        };
+      };
+
+      browsers = {
+        default = "chromium";
+        chromium.enable = true;
+      };
+    };
+
+    editors = {
+      default = "vim";
+      emacs.enable = true;
+      vim.enable = true;
+    };
+
+    development = {
+      cc.enable = true;
+      python.enable = true;
+      rust.enable = true;
+
+      go = {
+        enable = true;
+        path = "/data/1/opt/go";
+      };
+    };
+
+    media = {
+      spotify = {
+        enable = true;
+        daemon = {
+          enable = true;
+
+          settings = {
+            global = {
+              username = "22l46w473dznfqimcwcetx4sa";
+              password_cmd = "pass show spotify/macunha";
+            };
+          };
+        };
+      };
+    };
+
+    networking = {
+      kubernetes = {
+        enable = true;
+
+        helm.enable = true;
+      };
+
+      aws = {
+        enable = true;
+        iamAuthenticator.enable = true;
+      };
+
+      gcp.enable = true;
+      terraform.enable = true;
+    };
+
+    shell = {
+      zsh.enable = true;
+      fzf.enable = true;
+      tmux.enable = true;
+      pass.enable = true;
+      gnupg.enable = true;
+      direnv.enable = true;
+      asdf.enable = true;
+    };
+  };
+}
---
+++ hosts/orbit/networking.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  networking.hostName = "orbit";
+  networking.networkmanager.enable = true;
+
+  # DHCP
+  networking.useDHCP = false; # DEPRECATED: therefore, explicitly set to false
+  networking.interfaces = {
+    # Per-interface useDHCP is the new black
+    enp0s31f6.useDHCP = true;
+    wlp4s0.useDHCP = true;
+  };
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  networking.firewall = {
+    enable = true;
+    checkReversePath = false;
+
+    # allowedUDPPorts = [ ... ];
+    # allowedTCPPorts = [ ... ];
+  };
+}
